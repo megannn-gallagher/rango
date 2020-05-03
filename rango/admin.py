9ce9488424ee717e46766139bcc52352d35e196b
Megan Gallagher <2455723g@student.gla.ac.uk>
2020-05-03 14:31:22 +0100

    chapter 9 and exercises done

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
+
+admin.site.register(UserProfile)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -31,6 +31,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = 'rango:login'
+
 # Application definition
 
 INSTALLED_APPS = [
